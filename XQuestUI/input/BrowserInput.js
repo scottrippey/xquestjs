9825fd773818e155ac4f08fea88a0c0c5e54da79
Scott Rippey <scott.william.rippey@gmail.com>
2013-09-12 13:37:17 -0700

    Refactor - Removed dead code

--- XQuestUI/input/BrowserInput.js
+++ XQuestUI/input/BrowserInput.js
@@ -1,63 +1,7 @@
 var BrowserInput = new Class({
-
-
 	initialize: function(canvas) {
-		this.canvas = canvas;
-		this.document = canvas.getDocument();
-
-		//this._setupEvents();
-
 		this.inputItems = [];
 	}
-	, _setupEvents: function(){
-		this._onMouseDrag = this._onMouseDrag.bind(this);
-		this.document.addEvents({
-			'mousedown': this._onMouseDown.bind(this)
-			,'mouseup': this._onMouseUp.bind(this)
-
-			,'click': this._preventDefault
-			,'contextmenu': this._preventDefault
-		});
-	}
-	, _preventDefault: function(ev) {
-		ev.preventDefault();
-	}
-	, _onMouseDown: function(ev) {
-		ev.preventDefault();
-		if (!this.firstDown) {
-			this.firstDown = ev.getMouseButton();
-			this.engage();
-
-			this.dragStart = ev.client;
-			this.document.addEvent('mousemove', this._onMouseDrag);
-			this.document.body.addClass('mouse-engaged');
-		} else {
-			this.primaryWeapon();
-		}
-	}
-	, _onMouseUp: function(ev) {
-		ev.preventDefault();
-
-		var button = ev.getMouseButton();
-		if (this.firstDown === button) {
-			this.disengage();
-			this.firstDown = null;
-
-			this.document.removeEvent('mousemove', this._onMouseDrag);
-			this.document.body.removeClass('mouse-engaged');
-		}
-	}
-	, _onMouseDrag: function(ev) {
-		var sensitivity = 3;
-		var acceleration = {
-			x: (ev.client.x - this.dragStart.x) * sensitivity
-			, y: (ev.client.y - this.dragStart.y) * sensitivity
-		};
-		this.accelerate(acceleration);
-
-		this.dragStart = ev.client;
-	}
-
 
 	, processInputs: function(inputHandler) {
 		var notHandled = function(item) { return !inputHandler(item); };
