a4455d06b1d3c4cda31c0f14a6475ce63187402d
Scott Rippey <scott.william.rippey@gmail.com>
2013-09-15 01:09:10 -0700

    Refactor - renamed BrowserInput to GameInput

---
+++ XQuestGame/components/GameInput.js
@@ -0,0 +1,32 @@
+var GameInput = new Class({
+	initialize: function() {
+		this.inputItems = [];
+	}
+
+	, processInputs: function(inputHandler) {
+		var notHandled = function(item) { return !inputHandler(item); };
+		var unhandled = this.inputItems.filter(notHandled);
+		this.inputItems = unhandled;
+
+        if (unhandled.length !== 0)
+		    console.error("[GameInput]", "For now, all inputs should be handled.");
+	}
+
+	, engage: function() {
+		this.inputItems.push({ inputType: 'engage' });
+	}
+	, disengage: function() {
+		this.inputItems.push({ inputType: 'disengage' });
+	}
+	, primaryWeapon: function() {
+		this.inputItems.push({ inputType: 'primaryWeapon' });
+	}
+	, accelerate: function(acceleration) {
+		this.inputItems.push({
+			inputType: 'accelerate'
+			, x: acceleration.x
+			, y: acceleration.y
+		});
+	}
+
+});
