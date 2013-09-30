9a7dbec4ed45439f7e0c283d4b2e2c0b617c85ad
Scott Rippey <Scott.William.Rippey@gmail.com>
2013-09-30 10:33:59 -0700

    Balance - Changed the concept of "game mode" into separate, explicit override options

--- XQuestGame/options/Balance.js
+++ XQuestGame/options/Balance.js
@@ -1,27 +1,16 @@
+/**
+ * `Balance` defines all the variables that affect game play.
+ * This includes everything from size, speed, timing, and quantity.
+ * The values may differ depending on game mode or difficulty settings.
+ */
 var Balance = {
-	setGameMode: function(mode) {
-		if (!this._onUpdate) {
-			 return;
-		}
-		_.forEach(this._onUpdate, function(callback) {
-			callback(mode);
-		});
+	merge: function(values) {
+		_.merge(Balance, values);
 	}
-	,
-	onUpdate: function(callback) {
-		if (!this._onUpdate) {
-			this._onUpdate = [ callback ];
-		} else {
-			this._onUpdate.push(callback);
-		}
-	}
-	,
-	dependsOn: function(mode, modeValues) {
-		if (mode in modeValues)
-			return modeValues[mode];
-		return modeValues['default'];
-	}
-	,
+};
+
+// Helpers:
+Balance.merge({
 	/** @return {Function} that returns a random integer between min and max (inclusively) */
 	randomBetween: function(min, max) {
 		return function() {
@@ -35,64 +24,95 @@ var Balance = {
 			return (min + Math.random() * (max - min));
 		};
 	}
-};
-
-Balance.onUpdate(function(mode) {
-	_.merge(Balance, {
-		player: {
-			radius: 12
-			,looseFriction: 0.8
-			,bounceDampening : 0.3
-		}
-		,bullets: {
-			radius: 2
-			,speed: 3 // * player speed
-		}
-		,crystals: {
-			radius: 10
-			,quantity: Balance.dependsOn(mode, {
-				'default': 12
-				,'test': 3
-			})
-		}
-		,enemies: {
-			maxRadius: 13
-			,spawnRate: Balance.dependsOn(mode, {
-				'default': Balance.randomBetween(3, 6)
-				,'test': Balance.randomFloatBetween(1, 1)
-			})
-			,spawnDifficulty: 1.5 // Causes more difficult enemies to spawn more frequently
-			,splat: {
-				radius: 13
-				,speed: 40
-				,movementInterval: Balance.randomBetween(3, 10)
-			}
-			,locust: {
-				radius: 11
-				,speed: 150
-				,movementInterval: Balance.randomBetween(3, 5)
-				,turnSpeed: Balance.randomBetween(-100, 100)
-			}
-		}
-		,level: {
-			bounds: (function(){
-				var padding = 40
-					, visibleWidth = Balance.dependsOn(mode, { 'test': 1200, 'default': 800 })
-					, visibleHeight = Balance.dependsOn(mode, { 'test': 675, 'default': 450 })
-					, levelWidth = 1200, levelHeight = 675;
-				return {
-					x: padding, y: padding
-					, visibleWidth: padding + visibleWidth + padding
-					, visibleHeight: padding + visibleHeight + padding
-					, width: levelWidth
-					, height: levelHeight
-					, totalWidth: padding + levelWidth + padding
-					, totalHeight: padding + levelHeight + padding
-				};
-			})()
-		}
-		,gate: {
-			startingWidth: 150
-		}
-	});
 });
+
+// Here are all the game's options:
+Balance.merge({
+	gameModeOverrides: {
+		'arcade': { }
+		,
+		'test': {
+			fullView: true
+			,spawnRate: Balance.randomBetween(1, 1)
+			,crystalCount: 3
+		}
+	}
+	,
+	setGameMode: function(gameMode) {
+		var gameOptions = (typeof gameMode === 'object') ? gameMode : Balance.gameModeOverrides[gameMode] || {};
+
+		Balance.merge({
+			player: {
+				radius: 12
+				,looseFriction: 0.8
+				,bounceDampening : 0.3
+			}
+			,bullets: {
+				radius: 2
+				,speed: 3 // * player speed
+			}
+			,crystals: {
+				radius: 10
+				,quantity: gameOptions.crystalCount || 12
+			}
+			,enemies: {
+				maxRadius: 13
+				,spawnRate: gameOptions.spawnRate || Balance.randomBetween(3, 6)
+				,spawnDifficulty: 1.5 // Causes more difficult enemies to spawn more frequently
+				,splat: {
+					radius: 13
+					,speed: 40
+					,movementInterval: Balance.randomBetween(3, 10)
+				}
+				,locust: {
+					radius: 11
+					,speed: 150
+					,movementInterval: Balance.randomBetween(3, 5)
+					,turnSpeed: Balance.randomBetween(-100, 100)
+				}
+			}
+			,level: {
+				bounds: (function(){
+					var padding = 40
+						, visibleWidth = gameOptions.fullView ? 1200 : 800
+						, visibleHeight = gameOptions.fullView ? 675 : 450
+						, levelWidth = 1200, levelHeight = 675;
+					return {
+						x: padding, y: padding
+						, visibleWidth: padding + visibleWidth + padding
+						, visibleHeight: padding + visibleHeight + padding
+						, width: levelWidth
+						, height: levelHeight
+						, totalWidth: padding + levelWidth + padding
+						, totalHeight: padding + levelHeight + padding
+					};
+				})()
+			}
+			,gate: {
+				startingWidth: 150
+			}
+		});
+
+		this._fireUpdate(gameOptions);
+	}
+});
+
+// Events:
+Balance.merge({
+	onUpdate: function(callback) {
+		if (!this._onUpdate) {
+			this._onUpdate = [ callback ];
+		} else {
+			this._onUpdate.push(callback);
+		}
+	}
+	,
+	_fireUpdate: function(gameOptions) {
+		if (this._onUpdate) {
+			_.forEach(this._onUpdate, function(callback) {
+				callback(gameOptions);
+			});
+		}
+	}
+});
+
