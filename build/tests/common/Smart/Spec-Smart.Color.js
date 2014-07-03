7d78367a5d6760de1ac026b7f5e81b50d953d1a6
Scott Rippey <scott.william.rippey@gmail.com>
2014-07-03 00:17:35 -0700

    [Tests] toRGB

--- build/tests/common/Smart/Spec-Smart.Color.js
+++ build/tests/common/Smart/Spec-Smart.Color.js
@@ -32,4 +32,27 @@ describe("Smart.Color", function() {
 			expect(parseRGB('rgba(0, 0, 0, 0, 0)')).toEqual(null);
 		});
 	});
+	describe("toRGB", function() {
+		var toRGB = Smart.Color.toRGB;
+		it("should return rgb and rgba colors", function() {
+			expect(toRGB([ 255, 255, 255 ])).toBe('rgb(255,255,255)');
+			expect(toRGB([ 0, 0, 0 ])).toBe('rgb(0,0,0)');
+			expect(toRGB([ 0, 0, 255 ])).toBe('rgb(0,0,255)');
+			expect(toRGB([ 255, 0, 255 ])).toBe('rgb(255,0,255)');
+			
+			expect(toRGB([ 255, 255, 255, 0 ])).toBe('rgba(255,255,255,0.00)');
+			expect(toRGB([ 255, 255, 255, 0.001 ])).toBe('rgba(255,255,255,0.00)');
+			expect(toRGB([ 255, 255, 255, 0.01 ])).toBe('rgba(255,255,255,0.01)');
+			expect(toRGB([ 255, 255, 255, 0.1 ])).toBe('rgba(255,255,255,0.10)');
+			expect(toRGB([ 255, 255, 255, 0.99 ])).toBe('rgba(255,255,255,0.99)');
+			expect(toRGB([ 255, 255, 255, 0.999 ])).toBe('rgba(255,255,255,1.00)');
+		});
+		it("should validate and cap the values", function() {
+			expect(toRGB([ 256, 300, 9999999 ])).toBe('rgb(255,255,255)');
+			expect(toRGB([ -1, -300, -999999 ])).toBe('rgb(0,0,0)');
+			
+			expect(toRGB([ 256, 256, 256, 1.1 ])).toBe('rgba(255,255,255,1.00)');
+			expect(toRGB([ -1, -1, -1, -1 ])).toBe('rgba(0,0,0,0.00)');
+		});
+	});
 });
