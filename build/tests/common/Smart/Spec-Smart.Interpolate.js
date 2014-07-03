3b2681dc483e79cde96f15f156f778d2df5304aa
Scott Rippey <scott.william.rippey@gmail.com>
2014-07-03 01:21:47 -0700

    [Tests] Interpolate colors

--- build/tests/common/Smart/Spec-Smart.Interpolate.js
+++ build/tests/common/Smart/Spec-Smart.Interpolate.js
@@ -1,6 +1,7 @@
 describe("Smart.Interpolate", function() {
 	describe("Smart.Interpolate.numbers", function() {
-		var interpolate = Smart.Interpolate.numbers(100, 200);
+		var interpolateNumbers = Smart.Interpolate.numbers;
+		var interpolate = interpolateNumbers(100, 200);
 		
 		it("should return a function that accepts 1 parameter", function() {
 			expect(typeof interpolate).toBe('function');
@@ -12,16 +13,97 @@ describe("Smart.Interpolate", function() {
 				.toEqual([ 100, 125, 150, 175, 200 ]);
 		});
 
-		var interpolateNeg = Smart.Interpolate.numbers(-200, -100);
+		var interpolateNeg = interpolateNumbers(-200, -100);
 		it("should interpolate between negative values", function() {
 			expect([ 0.00, 0.25, 0.50, 0.75, 1.00 ].map(interpolateNeg))
 				.toEqual([ -200, -175, -150, -125, -100 ]);
 		});
 		
-		var interpolateDown = Smart.Interpolate.numbers(200, 100);
+		var interpolateDown = interpolateNumbers(200, 100);
 		it("should interpolate from high to low", function() {
 			expect([ 0.00, 0.25, 0.50, 0.75, 1.00 ].map(interpolateDown))
 				.toEqual([ 200, 175, 150, 125, 100 ]);
 		});
+		
+		it("should EXTRAPOLATE when given high and low pct", function() {
+			expect([ -1, -0.25, 0.5, 1.25, 2 ].map(interpolate))
+				.toEqual([ 0, 75, 150, 225, 300 ]);
+		})
+	});
+	describe("Smart.Interpolate.colors", function() {
+		var interpolateColors = Smart.Interpolate.colors;
+		var interpolate = interpolateColors('rgb(100, 110, 120)', 'rgb(200, 210, 220)');
+		
+		it("should interpolate between colors", function() {
+			expect([ 0, 0.25, 0.5, 0.75, 1 ].map(interpolate))
+				.toEqual([ 
+					'rgb(100,110,120)'
+					,'rgb(125,135,145)'
+					,'rgb(150,160,170)'
+					,'rgb(175,185,195)'
+					,'rgb(200,210,220)'
+				]);
+		});
+		var interpolateCap = interpolateColors('rgb(100,100,100)', 'rgb(200,200,200)');
+		it("should validate and cap values", function() {
+			expect([ -2, -1, -0.25, -0.1, 1.1, 1.5, 2 ].map(interpolateCap))
+				.toEqual([
+					'rgb(0,0,0)'
+					,'rgb(0,0,0)'
+					,'rgb(75,75,75)'
+					,'rgb(90,90,90)'
+					,'rgb(210,210,210)'
+					,'rgb(250,250,250)'
+					,'rgb(255,255,255)'
+				]);
+		});
+		
+		var interpolateAlpha = interpolateColors('rgba(0,0,0,0.1)', 'rgba(0,0,0,0.2)');
+		it("should interpolate alphas", function() {
+			expect([-1, -0.1, 0, 0.1, 0.5, 0.9, 1, 2].map(interpolateAlpha))
+				.toEqual([
+					'rgba(0,0,0,0.00)'
+					,'rgba(0,0,0,0.09)'
+					,'rgba(0,0,0,0.10)'
+					,'rgba(0,0,0,0.11)'
+					,'rgba(0,0,0,0.15)'
+					,'rgba(0,0,0,0.19)'
+					,'rgba(0,0,0,0.20)'
+					,'rgba(0,0,0,0.30)'
+				]);
+		});
+		
+		var interpolateHSL = interpolateColors('hsl(10, 20%, 30%)', 'hsl(20, 30%, 40%)');
+		it("should interpolate and cap hsl values too", function() {
+			expect([ -100, -2, -1, -0.1, 0, 0.1, 0.5, 0.9, 1, 1.1, 2, 100 ].map(interpolateHSL))
+				.toEqual([
+					'hsl(90,0%,0%)'
+					,'hsl(350,0%,10%)'
+					,'hsl(0,10%,20%)'
+					,'hsl(9,19%,29%)'
+					,'hsl(10,20%,30%)'
+					,'hsl(11,21%,31%)'
+					,'hsl(15,25%,35%)'
+					,'hsl(19,29%,39%)'
+					,'hsl(20,30%,40%)'
+					,'hsl(21,31%,41%)'
+					,'hsl(30,40%,50%)'
+					,'hsl(290,100%,100%)'
+				]);
+		});
+		var interpolateHSLA = interpolateColors('hsla(0,0%,0%,0.1)', 'hsla(0,0%,0%,0.2)');
+		it("should interpolate hsla", function() {
+			expect([ -100, -1, 0, 0.5, 1, 2, 100 ].map(interpolateHSLA))
+				.toEqual([
+					'hsla(0,0%,0%,0.00)'
+					,'hsla(0,0%,0%,0.00)'
+					,'hsla(0,0%,0%,0.10)'
+					,'hsla(0,0%,0%,0.15)'
+					,'hsla(0,0%,0%,0.20)'
+					,'hsla(0,0%,0%,0.30)'
+					,'hsla(0,0%,0%,1.00)'
+				]);
+		});
+		
 	});
 });
