81ab4ef7459a216a49aefa066a954d6524777445
Scott Rippey <scott.william.rippey@gmail.com>
2014-07-04 15:34:54 -0700

    [Drawing] Added setup method

--- XQuestGraphics/EaselJS/utils/Drawing.js
+++ XQuestGraphics/EaselJS/utils/Drawing.js
@@ -1,13 +1,41 @@
 EaselJSGraphics.Drawing = Smart.Class(new createjs.DisplayObject(), {
-	DisplayObject_initialize: createjs.DisplayObject.prototype.initialize
+	/**
+	 * When overridden, allows you to perform initialization tasks.
+	 * Constructor arguments will be passed.
+	 * @function
+	 * @param {*...} args
+	 */
+	setup: null
+	,
+	/**
+	 * When overridden, creates a DrawingQueue to create a static drawing. 
+	 * @function
+	 * @param {DrawingQueue} drawing
+	 */
+	drawStatic: null
+	,
+	/**
+	 * When overridden, this is called each tick, with a DrawingContext and a tickEvent.
+	 * @function
+	 * @param {DrawingContext} drawing
+	 * @param {TickEvent} tickEvent
+	 */
+	drawEffects: null
+
+
+	,DisplayObject_initialize: createjs.DisplayObject.prototype.initialize
 	,DisplayObject_draw: createjs.DisplayObject.prototype.draw
 	,sharedDrawingContext: new EaselJSGraphics.DrawingContext(null)
-	,initialize: function() {
-		this.Drawing_initialize();
+	,initialize: function(args_) {
+		this.Drawing_initialize.apply(this, arguments);
 	}
-	,Drawing_initialize: function() {
+	,Drawing_initialize: function(args_) {
 		this.DisplayObject_initialize();
 
+		if (this.setup) {
+			this.setup.apply(this, arguments);
+		}
+		
 		if (this.drawStatic) {
 			this.drawingQueue = new EaselJSGraphics.DrawingQueue();
 			this.drawStatic(this.drawingQueue);
@@ -29,7 +57,4 @@ EaselJSGraphics.Drawing = Smart.Class(new createjs.DisplayObject(), {
 
 		return true;
 	}
-
-	,drawStatic: null // Should be overridden
-	,drawEffects: null // Should be overridden
 });
