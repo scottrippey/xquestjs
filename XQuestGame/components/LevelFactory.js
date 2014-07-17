a4ea60f07d4d597bdbfd59ae8cef3ea82a040c10
Scott Rippey <scott.william.rippey@gmail.com>
2014-07-16 23:39:09 -0700

    [Level] Refactored level logic to LevelFactory

--- XQuestGame/components/LevelFactory.js
+++ XQuestGame/components/LevelFactory.js
@@ -4,21 +4,25 @@ XQuestGame.LevelFactory = Smart.Class({
 		
 		this.game.onNewLevel(this._onNewLevel.bind(this));
 	},
-	_onNewLevel: function(level) {
+	_onNewLevel: function() {
 		var currentLevel = this.game.currentLevel;
-		var currentEnemyLineupIndex = Math.floor(currentLevel / 2);
+		
+		this._setAlternatingEnemyPool(currentLevel);
+	},
+	_setAlternatingEnemyPool: function(currentLevel) {
+		var enemyLineup = Balance.enemies.roster;
 
-		var enemyLineup = Balance.enemies.roster, enemyPool;
-
-		if (currentEnemyLineupIndex >= enemyLineup.length) {
+		var alternatingLevel = Math.floor(currentLevel / 2)
+			, enemyPool;
+		if (alternatingLevel >= enemyLineup.length) {
 			// Very high levels include all enemies:
 			enemyPool = enemyLineup;
 		} else if ((currentLevel % 2) === 0) {
 			// Even levels only include the current enemy:
-			enemyPool = [ enemyLineup[currentEnemyLineupIndex] ];
+			enemyPool = [ enemyLineup[alternatingLevel] ];
 		} else {
 			// Odd levels include a variety of enemies, up to the current level index:
-			enemyPool = enemyLineup.slice(0, currentEnemyLineupIndex + 1);
+			enemyPool = enemyLineup.slice(0, alternatingLevel + 1);
 		}
 		
 		this.game.enemyFactory.setEnemyPool(enemyPool);
