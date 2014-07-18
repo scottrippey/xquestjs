0922394262955c2c5789bbb1ed41a46c7a154a80
Scott Rippey <scott.william.rippey@gmail.com>
2014-07-17 23:11:04 -0700

    [Enemy] [Locust] Increased size of locust

--- XQuestGraphics/EaselJS/characters/enemies/LocustGraphics.js
+++ XQuestGraphics/EaselJS/characters/enemies/LocustGraphics.js
@@ -1,14 +1,14 @@
 Balance.onUpdate(function(mode) {
 	var outerRadius = Balance.enemies.locust.radius
 		, outerOffset = 0
-		, innerRadius = outerRadius * 6 / 11
-		, innerOffset = outerRadius * 3 / 11;
+		, innerRadius = outerRadius * 7 / 13
+		, innerOffset = outerRadius * 4 / 13;
 	var orange = 'hsl(40, 100%, 50%)', red = 'hsl(0, 100%, 30%)';
 
 	_.merge(Graphics, {
 		enemies: {
 			locust: {
-				visibleRadius: Balance.enemies.locust.radius + 1
+				visibleRadius: outerRadius + 1
 				,triangleTriangle: {
 					outerTriangle: Smart.Drawing.polygonFromAngles(0, outerOffset, outerRadius, [ 0, 130, 230 ])
 					,outerStyle: { fillStyle: orange }
