3a5337e50fa53cae302c7260e887640269631a67
Scott Rippey <scott.william.rippey@gmail.com>
2025-07-15 20:21:58 -0600

    chore: added eslint/prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// eslint config in ESM format
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
+};
+
