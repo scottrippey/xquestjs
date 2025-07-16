bbba4094fbabd381f9fabc586e61f4b4db19c0fb
Scott Rippey <scott.william.rippey@gmail.com>
2025-07-16 02:55:07 -0600

    chore(import): added eslint-import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import globals from "globals";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
+import eslintPluginImport from "eslint-plugin-import";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 const myGlobals = {
@@ -33,10 +34,23 @@ export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
     languageOptions: { globals: myGlobals },
+    plugins: { import: eslintPluginImport },
     rules: {
       "no-unused-vars": "off",
       "no-var": "error",
       "prefer-const": "error",
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          pathGroupOverrides: [
+            {
+              pattern: "@/**",
+              action: "enforce",
+            },
+          ],
+        },
+      ],
     },
   },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
